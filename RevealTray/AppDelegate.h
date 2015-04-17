e9b63056b971e012b1088836723575ca40bdb642
murraysagal <murraysagal@mac.com>
2015-04-17 11:39:51 -0500

    Initial commit.

---
+++ RevealTray/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RevealTray
+//
+//  Created by Murray Sagal on 2015-04-17.
+//  Copyright (c) 2015 Murray Sagal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RevealTray/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RevealTray
+//
+//  Created by Murray Sagal on 2015-04-17.
+//  Copyright (c) 2015 Murray Sagal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
