6744afd90a439348a141d2f26453f91f7a2501bf
murraysagal <murraysagal@mac.com>
2015-04-20 10:16:39 -0500

    Presenting a view controller with the scoreboard, no code.

--- RevealTray/ViewController.m
+++ RevealTray/ViewController.m
@@ -14,14 +14,9 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"RevealTraySegueIdentifier"]) {
+        
+    }
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
